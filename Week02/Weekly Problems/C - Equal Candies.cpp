9454e0dc16547deb625cf9327bda0d48476736e1
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-10-06 20:17:57 +0600

    Create C - Equal Candies.cpp

---
+++ Week02/Weekly Problems/C - Equal Candies.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t; cin>>t;
+  while(t--){
+    int n; cin>>n;
+    int a[n];
+    int mn = INT_MAX;
+    int sum = 0;
+    for(int i = 0; i < n ; i++){
+      cin>>a[i];
+      sum +=a[i];
+      mn = min(mn, a[i]);
+    }
+    int val = sum - (n * mn);
+    cout<<val<<endl;
+  }
+  return 0;
+}
