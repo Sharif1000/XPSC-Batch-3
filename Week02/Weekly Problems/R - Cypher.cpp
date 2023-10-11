1c9c0a10d1d23a7d5300aec5628991ff1afc6a60
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-10-11 09:46:28 +0600

    Create R - Cypher.cpp

---
+++ Week02/Weekly Problems/R - Cypher.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  int t; cin>>t;
+  while(t--){
+    int n; cin>>n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+      cin>>a[i];
+    }
+    for(int i = 0; i < n; i++){
+      int x; cin>>x;
+      string s; cin>>s;
+      for(char c:s){
+        if(c == 'D') a[i]++;
+        else a[i]--;
+      }
+      if(a[i] > 9){
+        a[i] =a[i]-10;
+      }
+      else if(a[i] < 0){
+        a[i] = a[i] +10;
+      }
+    }
+    for(int i = 0; i < n; i++){
+      cout<<a[i]<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
