5336b5d600fab42d69df283b6b92eb2febad3e4a
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-10-11 09:48:35 +0600

    Update R - Cypher.cpp

--- Week02/Weekly Problems/R - Cypher.cpp
+++ Week02/Weekly Problems/R - Cypher.cpp
@@ -7,9 +7,7 @@ int main()
   while(t--){
     int n; cin>>n;
     int a[n];
-    for(int i = 0; i < n; i++){
-      cin>>a[i];
-    }
+    for(int i = 0; i < n; i++) cin>>a[i];
     for(int i = 0; i < n; i++){
       int x; cin>>x;
       string s; cin>>s;
@@ -17,12 +15,8 @@ int main()
         if(c == 'D') a[i]++;
         else a[i]--;
       }
-      if(a[i] > 9){
-        a[i] =a[i]-10;
-      }
-      else if(a[i] < 0){
-        a[i] = a[i] +10;
-      }
+      if(a[i] > 9) a[i] =a[i]-10;
+      else if(a[i] < 0) a[i] = a[i] +10;
     }
     for(int i = 0; i < n; i++){
       cout<<a[i]<<" ";
