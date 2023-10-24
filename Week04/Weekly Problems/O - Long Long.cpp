dcaf2abda65877a29aa98519f1063a2c876f4e88
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-10-24 18:52:45 +0600

    Update O - Long Long.cpp

--- Week04/Weekly Problems/O - Long Long.cpp
+++ Week04/Weekly Problems/O - Long Long.cpp
@@ -3,32 +3,22 @@ using namespace std;
 
 int main() 
 {
-    int t;
-    cin>>t;
+    int t; cin>>t;
     while(t--){
-      int n;
-      cin>>n;
+      int n; cin>>n;
       int arr[n];
-      for(int i = 0; i < n; i++){
-        cin>>arr[i];
-      }
+      for(int i = 0; i < n; i++) cin>>arr[i];
       int cnt = 0;
       long long int sum = 0;
       
       for(int i = 0; i < n; i++){
-        if(arr[i] >=0){
-          continue;
-        } 
+        if(arr[i] >=0) continue;
         int j = i;
-        while(j < n && arr[j] <= 0){
-          j++;
-        }
+        while(j < n && arr[j] <= 0) j++;
         cnt++;
         i = j -1; 
       }
-      for(int i = 0; i < n; i++){
-        sum += abs(arr[i]);
-      }
+      for(int i = 0; i < n; i++) sum += abs(arr[i]);
       cout<<sum<<" "<<cnt<<endl;
     }
     return 0;
