0b572cc15d442041451eddad998e0bf1a37c2ed2
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-06 20:34:55 +0600

    Create Problem06.cpp

---
+++ Week03/Weekly Topic/Problem06.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t; cin>>t;
+  while(t--){
+    int ans = 0;
+    int n; cin>>n;
+    int x;
+    for(int i = 0; i < n; i++){
+      cin>>x;
+      ans ^=x;
+    }
+    if(ans == 0 && n % 2 == 0){
+      cout<<0<<endl;
+    }
+    else if(n % 2 == 1){
+      cout<<ans<<endl;
+    }
+    else{
+      cout<<-1<<endl;
+    }
+  }
+  return 0;
+}
