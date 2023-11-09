bf1bc4b54ba5611274ec9dcbbaa03327accc8733
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-09 15:55:29 +0600

    Create Q - Bus Seat Numbering.cpp

---
+++ Week06/Weekly Problems/Q - Bus Seat Numbering.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    if(n <= 10) cout<<"Lower Double"<<endl;
+	    else if(n > 10 && n < 16) cout<<"Lower Single"<<endl;
+	    else if(n > 15 && n < 26) cout<<"Upper Double"<<endl;
+	    else cout<<"Upper Single"<<endl;
+	}
+	return 0;
+}
