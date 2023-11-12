c7470a0a24526984f0e2faa6ec8e81590c75b1d6
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-12 19:30:42 +0600

    Create M - Masterchef finals.cpp

---
+++ Week07/Weekly Problems/M - Masterchef finals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    int x;
+	    cin>>x;
+	    (x <= 10) ? cout<<"YES":cout<<"NO";
+	    cout<<endl;
+	}
+	return 0;
+}
