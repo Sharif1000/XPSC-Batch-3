9e67693835d95e3ed88d720acf3fc2529965035f
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-12 19:16:53 +0600

    Create K - Parliament.cpp

---
+++ Week07/Weekly Problems/K - Parliament.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n <= (k*2)) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
