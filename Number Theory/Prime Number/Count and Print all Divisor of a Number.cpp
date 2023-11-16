b651dd5ae253667ab5741c991abdc4943546d6d3
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-16 19:49:33 +0600

    Update Count and Print all Divisor of a Number.cpp

--- Number Theory/Prime Number/Count and Print all Divisor of a Number.cpp
+++ Number Theory/Prime Number/Count and Print all Divisor of a Number.cpp
@@ -8,18 +8,19 @@ int main()
 	cin>>t;
 	while(t--){
 	  ll n; cin>>n;
-	  set<ll>s;
+	  vector<ll>v;
 	  for(ll i = 1; i * i <= n; i++){
 	    if(n % i == 0){
-	      s.insert(i);
-	      s.insert(n/i);
+	      v.push_back(i);
+	      if((n / i) != i) v.push_back(n/i);
 	    }
 	  }
+	  sort(v.begin(), v.end());
 	  //count the number of divisor
-	  int sz = s.size();
+	  int sz = v.size();
 	  cout<<sz<<endl;
 	  //print all divisors
-	  for(ll val:s) cout<<val<<" ";
+	  for(ll val:v) cout<<val<<" ";
 	  cout<<endl;
 	}
 	return 0;
