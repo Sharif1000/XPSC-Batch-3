77a8c86e0515eed9a9fb00f62ad906bdd931e181
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-18 14:32:38 +0600

    Update M - Mathematical Addition.cpp

--- Week08/Weekly Problems (Super)/M - Mathematical Addition.cpp
+++ Week08/Weekly Problems (Super)/M - Mathematical Addition.cpp
@@ -1,1 +1,14 @@
-fgfgffg
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+  int t; cin>>t;
+  while(t--){
+    ll a,b;
+    cin>>a>>b;
+    cout<<-(a*a)<<" "<<(b*b)<<endl;
+  }
+  return 0;
+}
