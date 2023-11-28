6e5506ee154ce099380d4a033e3c7525154cfcbc
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-11-28 22:02:18 +0600

    Update A - Mocha and Math.cpp

--- Week09/Weekly Problems (Super)/A - Mocha and Math.cpp
+++ Week09/Weekly Problems (Super)/A - Mocha and Math.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long 
 
-int main() 
+int main()
 {
-    cout << "Hello, World!";
-    return 0;
+  int t; cin>>t;
+  while(t--){
+    int n; cin>>n;
+    int a[n];
+    for(int i = 0; i < n; i++) cin>>a[i];
+    int ans = a[0];
+    for(int i = 1; i < n; i++) ans = (ans & a[i]);
+    cout<<ans<<endl;
+  }
+  return 0;
 }
