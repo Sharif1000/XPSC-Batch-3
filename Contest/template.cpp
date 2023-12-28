11d11bb92f709750fe648fd3a7e40304a217d2c7
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2023-12-28 19:18:25 +0600

    Create template.cpp

---
+++ Contest/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define forl(i,a,b) for(int i = a; i < b; i++)
+#define en endl
+
+int main() 
+{
+  int t; cin>>t;
+  while(t--){
+    
+  }
+  return 0;
+}
