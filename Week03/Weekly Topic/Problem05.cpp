1caca583a37881121cccb51e366f3fe9ffa1a914
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-04 21:50:09 +0600

    Update Problem05.cpp

--- Week03/Weekly Topic/Problem05.cpp
+++ Week03/Weekly Topic/Problem05.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+  int t; cin>>t;
+  while(t--){
+    int n, k; cin>>n>>k;
+    long long int mod = 1e9+7;
+    long long int res = 1;
+    while(k>0){
+        res = (res * n)% mod;
+        k--;
+    }
+    cout<<res<<endl;
+  }
+  return 0;
+}
