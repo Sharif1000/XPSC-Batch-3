c99f19a8be0beb581de40fbec8b04b0d25362850
Md Shariful islam <103664786+Sharif1000@users.noreply.github.com>
2025-04-05 13:43:10 +0600

    Update Problem_02.cpp

--- Week03/Bit Manipulation/Problems/Problem_02.cpp
+++ Week03/Bit Manipulation/Problems/Problem_02.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main(){
   int t; cin>>t;
   while(t--){
-  int n,k; cin>>n>>k;
-  int ans = 0;
-  while(n > 0){
-    ans += n % k;
-    n /= k;
+    int n,k; cin>>n>>k;
+    int ans = 0;
+    if(k == 1) cout<<n<<endl;
+    else{
+      while(n > 0){
+      ans += n % k;
+      n /= k;
+    }
+    cout<<ans<<endl;
   }
-  cout<<ans<<endl;
+    
 }
   return 0;
 }
